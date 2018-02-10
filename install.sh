5ef58bffb0c85000d30606deff8670ed2c1215f5
Scott Bennett <scottbennett912@gmail.com>
2018-02-09 18:18:37 -0600

    Fixed some bugs with the install script. Downloads using git instead of svn

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+BASEDIR=$PWD
+
 ###############################
 #      Install LLVM(WASM)     #
 ###############################
@@ -7,32 +9,24 @@
 # NOTE: THIS WILL TAKE A WHILE... :(
 
 # locations, e.g.
-WORKDIR=llvm
-mkdir -p $WORKDIR
-INSTALLDIR=$WORKDIR
-cd $WORKDIR
+LLVMDIR=$BASEDIR/llvm
+mkdir -p $LLVMDIR
 
-# checkout LLVM
-svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+cd $LLVMDIR
+git clone https://github.com/llvm-mirror/llvm.git # checkout LLVM
 
-cd $WORKDIR/llvm/tools
-
-# checkout clang
-svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
-
-# checkout lld
-svn co http://llvm.org/svn/llvm-project/lld/trunk lld
+cd $LLVMDIR/llvm/tools
+git clone https://github.com/llvm-mirror/clang.git # checkout clang
+git clone https://github.com/llvm-mirror/lld.git # checkout lld
 
 # build folder (~14 min; ~1 hour /wo -j)
-mkdir -p $WORKDIR/llvm-build
-cd $WORKDIR/llvm-build
-# For Debug build:
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DLLVM_TARGETS_TO_BUILD="" -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly -DCMAKE_BUILD_TYPE=Release -DLLVM_INCLUDE_EXAMPLES=OFF $WORKDIR/llvm
-make -j 8
+mkdir -p $LLVMDIR/build
+cd $LLVMDIR/build
+
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$LLVMDIR -DLLVM_TARGETS_TO_BUILD="" -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly -DCMAKE_BUILD_TYPE=Release -DLLVM_INCLUDE_EXAMPLES=OFF $LLVMDIR/llvm && make -j 8
 
 # install llvm
 make install
-exit 0
 
 ###############################
 
@@ -41,10 +35,9 @@ exit 0
 #      Install binaryen       #
 ###############################
 
-#brew install binaryen
-cd ~
+cd $BASEDIR
 git clone https://github.com/WebAssembly/binaryen.git
-cd binaryen
+cd $BASEDIR/binaryen
 cmake . && make -j 8
 
 ###############################
